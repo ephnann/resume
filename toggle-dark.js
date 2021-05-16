818ec504673f03a3a67546dcae5266aa3a5c5c8d
Ephraim Nann <ephnann@gmail.com>
2021-05-16 15:35:50 +0930

    Dark mode

--- toggle-dark.js
+++ toggle-dark.js
@@ -1,47 +1,9 @@
-const prefersDark = window.matchMedia("(prefers-color-scheme: dark)")
-const body = document.body
-const savedTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null
-const btn = document.querySelector("#toggle-dark")
+const input = document.querySelector('.theme-switcher input');
 
-// Check for previously indicated dark/light preference
-
-if (savedTheme) {
-    document.documentElement.setAttribute('data-theme', savedTheme)
-    btn.innerHTML = savedTheme
-} else {
-    // Check for system preference and save for future
-    // ...currently somewhat of a workaround to avoid flash of unstyled content on page load for dark users
-    window.onload = setDataTheme = () => {
-        if (prefersDark.matches) {
-            document.documentElement.setAttribute('data-theme', 'dark')
-            localStorage.setItem('theme', 'dark')
-            console.log("Contrast mode " + document.documentElement.getAttribute('data-theme'))
-            btn.innerHTML = "dark"
-        } else {
-            document.documentElement.setAttribute('data-theme', 'light')
-            localStorage.setItem('theme', 'light')
-            console.log("Contrast mode set to " + document.documentElement.getAttribute('data-theme'))
-            btn.innerHTML = "light"
-        }
-    }
-}
-
-
-
-
-// Handle toggle via click
-btn.addEventListener("click", () => {
-    if (document.documentElement.getAttribute('data-theme') === 'dark') {
-        // body.classList.toggle("light-mode")
-        document.documentElement.setAttribute('data-theme', 'light')
-        localStorage.setItem('theme', 'light')
-        console.log("Contrast mode set to " + document.documentElement.getAttribute('data-theme'))
-        btn.innerHTML = "dark"
+input.addEventListener('change', (e) => {
+    if(e.target.checked) {
+        document.body.setAttribute('data-theme', 'dark');
     } else {
-        // body.classList.toggle("dark-mode")
-        document.documentElement.setAttribute('data-theme', 'dark')
-        localStorage.setItem('theme', 'dark')
-        console.log("Contrast mode set to " + document.documentElement.getAttribute('data-theme'))
-        btn.innerHTML = "light"
+        document.body.setAttribute('data-theme', 'light');
     }
 })
